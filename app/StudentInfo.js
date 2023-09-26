2f5f42f0295e7b7caa72c184fa6666fc43807891
jonahscott <jonah.scott@edu.sait.ca>
2023-09-25 21:38:05 -0600

    make student info look like live view demo

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -3,14 +3,13 @@ import Link from 'next/link';
 const StudentInfo = () => {
     return (
         <div>
-            <p>Jonah Scott</p>
-            <p>CPRG 306-A</p>
+            <p>Name: Jonah Scott</p>
+            <p>Course Section: CPRG 306-A</p>
             <Link href="https://github.com/jonahscott/cprg306-assignments" passHref>
-            My GitHub Repository
+                <a>My GitHub Repository</a>
             </Link>
         </div>
     );
 };
 
 export default StudentInfo;
-
