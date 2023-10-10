e6e246a4dc65f4df2d7bb7ad45e50122547fa65e
jonahscott <jonah.scott@edu.sait.ca>
2023-10-09 22:36:38 -0600

    redo new item, look nice, provide alerts

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -18,28 +18,64 @@ export default function NewItem() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="p-8 max-w-md mx-auto bg-white rounded-xl shadow-md space-y-4">
-      <div className="flex flex-col space-y-2">
-        <label htmlFor="name" className="text-sm font-medium">Name:</label>
-        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required className="p-2 border rounded-md" />
-      </div>
-      
-      <div className="flex flex-col space-y-2">
-        <label htmlFor="quantity" className="text-sm font-medium">Quantity:</label>
-        <input type="number" id="quantity" min="1" max="99" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} required className="p-2 border rounded-md" />
-      </div>
-
-      <div className="flex flex-col space-y-2">
-        <label htmlFor="category" className="text-sm font-medium">Category:</label>
-        <select id="category" value={category} onChange={(e) => setCategory(e.target.value)} className="p-2 border rounded-md">
-          <option value="produce">Produce</option>
-          <option value="dairy">Dairy</option>
-          <option value="bakery">Bakery</option>
-          {/* Add other categories as per your requirement */}
-        </select>
-      </div>
-
-      <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">Submit</button>
-    </form>
+    <div className="min-h-screen flex items-center justify-center bg-black-100">
+      <form onSubmit={handleSubmit} className="p-8 bg-white rounded-md shadow-md w-96 space-y-4">
+        <div>
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            Item Name
+          </label>
+          <input 
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            placeholder="Item Name"
+            className="mt-1 p-2 w-full border rounded-md text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="quantity" className="block text-sm font-medium text-gray-700">
+            Quantity
+          </label>
+          <input 
+            type="number"
+            id="quantity"
+            min="1"
+            max="99"
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+            required
+            className="mt-1 p-2 w-full border rounded-md text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="category" className="block text-sm font-medium text-gray-700">
+            Category
+          </label>
+          <select 
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="mt-1 p-2 w-full border rounded-md text-black"
+          >
+            <option value="produce">Produce</option>
+            <option value="dairy">Dairy</option>
+            <option value="bakery">Bakery</option>
+            <option value="meat">Meat</option>
+            <option value="frozen">Frozen Foods</option>
+            <option value="canned">Canned Goods</option>
+            <option value="dry">Dry Goods</option>
+            <option value="beverages">Beverages</option>
+            <option value="snacks">Snacks</option>
+            <option value="household">Household</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <button type="submit" className="py-2 px-4 bg-blue-500 text-white rounded">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
