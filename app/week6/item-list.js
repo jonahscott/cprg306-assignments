12a5f44a9191306928c7bf7afe7f203569f85e99
jonahscott <jonah.scott@edu.sait.ca>
2023-10-23 23:18:48 -0600

    fix itemlist

--- app/week6/item-list.js
+++ app/week6/item-list.js
@@ -1,38 +1,39 @@
-"use client";
-
-import React, { useState } from 'react';
 import Item from './item';
-import itemsData from './items.json';
+import { useState } from 'react';
 
-const ItemList = () => {
+const ItemList = ({ items }) => {
     const [sortBy, setSortBy] = useState('name');
-    const sortedItems = [...itemsData];
 
-    if (sortBy === 'name') {
-        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
-    } else if (sortBy === 'category') {
-        sortedItems.sort((a, b) => a.category.localeCompare(b.category));
-    }
+    const sortedItems = [...items].sort((a, b) => {
+        if (sortBy === 'name') {
+            return a.name.localeCompare(b.name);
+        } else if (sortBy === 'category') {
+            return a.category.localeCompare(b.category);
+        }
+        return 0;
+    });
 
     return (
         <div className="bg-gray-100 p-4 w-full min-h-screen">
-            <div className="mb-4">
-                <button
-                    className={`px-4 py-2 mr-2 ${sortBy === 'name' ? 'bg-blue-500 text-white' : 'bg-blue-200'}`}
-                    onClick={() => setSortBy('name')}
-                >
-                    Sort by Name
-                </button>
-                <button
-                    className={`px-4 py-2 ${sortBy === 'category' ? 'bg-blue-500 text-white' : 'bg-blue-200'}`}
-                    onClick={() => setSortBy('category')}
-                >
-                    Sort by Category
-                </button>
+            <div className="flex justify-between mb-4">
+                <div>
+                    <button 
+                        className={`p-2 m-2 ${sortBy === 'name' ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}
+                        onClick={() => setSortBy('name')}
+                    >
+                        Sort by Name
+                    </button>
+                    <button 
+                        className={`p-2 m-2 ${sortBy === 'category' ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}
+                        onClick={() => setSortBy('category')}
+                    >
+                        Sort by Category
+                    </button>
+                </div>
             </div>
             <ul>
-                {sortedItems.map(item => (
-                    <Item key={item.id} {...item} />
+                {sortedItems.map((item, index) => (
+                    <Item key={index} {...item} index={index} />
                 ))}
             </ul>
         </div>
