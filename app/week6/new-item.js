89ce2c1bc33aeb8fcaf516c262f3a84cb797723e
jonahscott <jonah.scott@edu.sait.ca>
2023-10-23 23:31:10 -0600

    update week6page.js/new-item to work
    fix page/new-item errors

--- app/week6/new-item.js
+++ app/week6/new-item.js
@@ -1,81 +1,56 @@
-"use client";
+import { useState } from 'react';
 
-import React, { useState } from 'react';
+const NewItem = ({ onAddItem }) => {
+    const [name, setName] = useState('');
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState('produce');
 
-export default function NewItem() {
-  const [name, setName] = useState('');
-  const [quantity, setQuantity] = useState(1);
-  const [category, setCategory] = useState('produce');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const item = {
+            name: name,
+            quantity: quantity,
+            category: category,
+        };
+        onAddItem(item);
+        setName('');
+        setQuantity(1);
+        setCategory('produce');
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const item = { name, quantity, category };
-    console.log(item);
-    alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
-    setName('');
-    setQuantity(1);
-    setCategory('produce');
-  };
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black-100">
-      <form onSubmit={handleSubmit} className="p-8 bg-white rounded-md shadow-md w-96 space-y-4">
-        <div>
-          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-            Item Name
-          </label>
-          <input 
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            placeholder="Item Name"
-            className="mt-1 p-2 w-full border rounded-md text-black"
-          />
+    return (
+        <div className="text-black">
+            <form onSubmit={handleSubmit}>
+                <div className="m-2">
+                    <label>Name:</label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} required className="border p-1 w-full" />
+                </div>
+                <div className="m-2">
+                    <label>Quantity:</label>
+                    <input type="number" min="1" max="99" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} required className="border p-1 w-full" />
+                </div>
+                <div className="m-2">
+                    <label>Category:</label>
+                    <select value={category} onChange={(e) => setCategory(e.target.value)} className="border p-1 w-full">
+                        <option value="produce">Produce</option>
+                        <option value="dairy">Dairy</option>
+                        <option value="bakery">Bakery</option>
+                        <option value="meat">Meat</option>
+                        <option value="frozen foods">Frozen Foods</option>
+                        <option value="canned goods">Canned Goods</option>
+                        <option value="dry goods">Dry Goods</option>
+                        <option value="beverages">Beverages</option>
+                        <option value="snacks">Snacks</option>
+                        <option value="household">Household</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <div className="m-2">
+                    <button type="submit" className="bg-blue-500 text-white p-2 rounded">Add Item</button>
+                </div>
+            </form>
         </div>
-        <div>
-          <label htmlFor="quantity" className="block text-sm font-medium text-gray-700">
-            Quantity
-          </label>
-          <input 
-            type="number"
-            id="quantity"
-            min="1"
-            max="99"
-            value={quantity}
-            onChange={(e) => setQuantity(Number(e.target.value))}
-            required
-            className="mt-1 p-2 w-full border rounded-md text-black"
-          />
-        </div>
-        <div>
-          <label htmlFor="category" className="block text-sm font-medium text-gray-700">
-            Category
-          </label>
-          <select 
-            id="category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="mt-1 p-2 w-full border rounded-md text-black"
-          >
-            <option value="produce">Produce</option>
-            <option value="dairy">Dairy</option>
-            <option value="bakery">Bakery</option>
-            <option value="meat">Meat</option>
-            <option value="frozen">Frozen Foods</option>
-            <option value="canned">Canned Goods</option>
-            <option value="dry">Dry Goods</option>
-            <option value="beverages">Beverages</option>
-            <option value="snacks">Snacks</option>
-            <option value="household">Household</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-        <button type="submit" className="py-2 px-4 bg-blue-500 text-white rounded">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-}
+    );
+};
+
+export default NewItem;
--- app/week6/page.js
+++ app/week6/page.js
@@ -1,13 +1,21 @@
+"use client";
+import { useState } from 'react';
+import NewItem from './new-item';
 import ItemList from './item-list';
+import itemsData from './items.json';
 
 function Page() {
+    const [items, setItems] = useState(itemsData);
+
+    const handleAddItem = (newItem) => {
+        setItems(prevItems => [...prevItems, newItem]);
+    };
+
     return (
         <main className="max-w-2xl mx-auto mt-12 p-4 bg-gray-200 shadow-md">
             <h1 className="text-4xl font-bold mb-6 text-black">Shopping List</h1>
-            <div className="mb-4">
-                <h2 className="text-2xl font-semibold mb-2 text-black">Sort by:</h2>
-                <ItemList />
-            </div>
+            <NewItem onAddItem={handleAddItem} />
+            <ItemList items={items} />
         </main>
     );
 }
